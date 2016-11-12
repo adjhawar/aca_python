85a51f4cd1eb8bacb5a15b2355ad9ebde52a12f9
Aditya Jhawar <adjhawar0697@gmail.com>
2016-11-13 03:55:16 +0530

    Add files via upload
    files uploaded

---
+++ project1.py
@@ -0,0 +1,16 @@
+repeat 3
+{
+shoot
+}
+$ x=4
+if x>2
+{
+shoot
+forward
+turnright
+}
+repeat 4
+{
+shoot
+backward
+}
---
+++ project2.py
@@ -0,0 +1,16 @@
+$ x=2
+repeat x
+{
+forward
+shoot
+}
+if 1>5
+{
+shoot
+backward
+}
+turnleft
+backward
+shoot
+forward
+shoot
